fbf105d428dc885f4e08a537ab43fa061c61232f
Vinay Joshi <vinay.joshi.ngi@gmail.com>
2014-01-11 20:00:12 +0530

    Vinay Joshi

---
+++ signalr_demo/App/CustomBindings.js
@@ -0,0 +1,9 @@
+﻿ko.bindingHandlers.displayMoney = {
+    init: function (element, valueAccessor, allBindingAccessor, viewModel, bindingContext) {
+        $(element).html('$' + valueAccessor().toFixed(2));
+    },
+
+    update: function (element, valueAccessor, allBindingAccessor, viewModel, BindingContext) {
+
+    }
+};
